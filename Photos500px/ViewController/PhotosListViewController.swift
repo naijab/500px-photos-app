9ac53e82d602e41a590bdabaf13adc98285d6818
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-05-16 03:30:36 +0700

    add footer for show loading

--- Photos500px/ViewController/PhotosListViewController.swift
+++ Photos500px/ViewController/PhotosListViewController.swift
@@ -84,10 +84,10 @@ final class PhotosListViewController: UIViewController {
 extension PhotosListViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let row = indexPath.row
+        let lastSectionIndex = tableView.numberOfSections - 1
+        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
 
-        if let photos = self.photos,
-           photos.count - 1 == row,
+        if indexPath.section == lastSectionIndex && indexPath.row == lastRowIndex,
            self.hasNextPage {
 
             self.photosListTableView.tableFooterView = spinner
